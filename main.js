70c23cc71b1035b01ef0ab075a75f2660a6d3e19
Samiullah <samiullah.khan.1989@gmail.com>
2017-08-29 16:51:52 +0500

    Now file open works and the rendere process can talk with the main process to load the file but then main process sends the data to renderere process. Validation is added to only accept .txt and .md files

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var { app, BrowserWindow } = require("electron");
+var { app, BrowserWindow, dialog } = require("electron");
+const fs = require('fs');
 
 let mainWindow = null
 
 app.on('ready',() => {
   mainWindow = new BrowserWindow({
-    width: 300,
+    width: 500,
     height: 600,
+    minWidth: 500,
     show: false
   });
 
@@ -15,8 +17,26 @@ app.on('ready',() => {
 
   mainWindow.on('closed', () => {
     mainWindow = null;
-  })
+  });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   // require('devtron').install();
-})
+});
+
+const getFileFromUserSelection = exports.getFileFromUserSelection = () => {
+  const files = dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [
+      {name: 'Text Files', extensions: ['txt', 'text']},
+      {name: 'Markdown Files', extensions: ['md', 'markdown']}
+    ]
+  });
+
+  if(!files) return;
+  const file = files[0];
+  const content = fs.readFileSync(file).toString();
+
+  mainWindow.webContents.send('file-opened', file, content);
+
+  console.log(content);
+}
--- renderer.js
+++ renderer.js
@@ -1,9 +1,29 @@
 const marked = require('marked');
+const { remote, ipcRenderer } = require('electron');
+const mainProcess = remote.require('./main');
 
-const markdownView       = document.getElementById('#view');
-const htmlView           = document.getElementById('#html');
-const newFileButton      = document.getElementById('#new-file');
-const openFileButton     = document.getElementById('#open-file');
-const saveMarkdownButton = document.getElementById('#save-file');
-const revertButton = document.getElementById('#revert');
-const saveHtmlButton = document.getElementById('#save-html');
+const markdownView       = document.getElementById('html');
+const htmlView           = document.getElementById('view');
+const newFileButton      = document.getElementById('new-file');
+const openFileButton     = document.getElementById('open-file');
+const saveMarkdownButton = document.getElementById('save-file');
+const revertButton = document.getElementById('revert');
+const saveHtmlButton = document.getElementById('save-html');
+
+
+const renderMarkdowToHtml = (markdown) => {
+  htmlView.innerHTML = marked(markdown, {sanitize: true});
+}
+
+markdownView.addEventListener('keyup', (event) => {
+  renderMarkdowToHtml(event.target.value);
+});
+
+openFileButton.addEventListener('click', ()=> {
+  mainProcess.getFileFromUserSelection();
+})
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  markdownView.value = content;
+  renderMarkdowToHtml(content);
+})
